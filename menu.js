ab19b44750530dc3da43bf6ab3f19a9a51cc8733
matiasborneman <matias_borneman@hotmail.com>
2023-02-15 22:33:20 -0300

    primer commit con el maquetado del proyecto cv argentina progama sin datos personales

---
+++ menu.js
@@ -0,0 +1,5 @@
+function mostrarInformacion(){
+    document.getElementById("container-body").style.display ='block';
+    document.getElementById("navbar").style.display ='block';
+    document.getElementById("container-principal").style.display ='none';
+}
